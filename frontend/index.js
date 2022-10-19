03abeb484ce591740698c0905b22ddcca83f2e1b
raul.alonso <raul.alonso@coches.com>
2022-10-19 13:58:49 +0200

    add cards

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,20 @@
-import {initializeBlock, Button, Input, Label, Loader, useBase} from '@airtable/blocks/ui';
+import {initializeBlock, Box,Icon, Button, Input, Label, Loader, RecordCard, useBase, useRecords} from '@airtable/blocks/ui';
 import React, { useState } from "react";
+
+const tableName = 'Web links';
+const tableNameAmenities = 'Amenities';
 
 function ScrapIdealista() {
     const base = useBase();
-    const [value, setValue] = useState("98444394");
+    const table = base.getTableByName(tableName);
+    const records = useRecords(table);
+    
+
+    const [value, setValue] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const [scraperMsg, setScraperMsg] = useState("");
     const [scraperResult, setScraperResult] = useState("");
+
     return <div >
         <br/><br/>
         <Label 
@@ -57,7 +65,112 @@ function ScrapIdealista() {
         {(scraperResult == "ok")? 
             <Label marginLeft="20px" textColor="green">Scraper completed correctly</Label>:null
         }
+        <br/>
+        <br/>
+        <ShowScrapedRecordsComponent records={records} table={table} />
         </div>;
+}
+
+function ShowScrapedRecordsComponent({records, table}) {
+    let fields = [
+        table.getField("Scraper Price"),
+        // table.getField("Scraper FractionPrice"),
+        table.getField("Scraper SQM"),
+        table.getField("Scraper Bedrooms"),
+        table.getField("Scraper Bathrooms"),
+        table.getField("Scraper Gallery"),
+        
+    ];
+    let scrapedRecordsFiltered = records ? filterScrperList(records) : null;
+    let scrapedRecords = scrapedRecordsFiltered ? scrapedRecordsFiltered.map(record => {
+        return (
+           <Box key={record.id}>
+                <RecordCard width={600} marginTop="7px" marginBottom="3px" record={record} fields={fields}/>
+                <Label>&#8364; Estimated Fraction Price: {record.getCellValue("Scraper FractionPrice").toLocaleString('en-US', {style: 'currency',currency: 'EUR', maximumFractionDigits: 0})}</Label>
+                {
+                    (record.getCellValue("Scraper IsNewConstruction"))?
+                        <Label marginLeft="15px">&#9961; New Construction</Label>
+                        :null
+                }
+                
+            </Box>
+        );
+    }): null;
+    return (
+        <Box marginLeft="20px">
+            <Box>Number of incomplete records scraped: {scrapedRecords.length}</Box>
+            {scrapedRecords}
+            <Box marginTop="10px" marginBottom="20px">
+                <Button onClick={async () => 
+                {
+                    dumpData(scrapedRecordsFiltered, table)
+                }} icon="thumbsUp">
+                     Sync Data
+                </Button> 
+            </Box>
+        </Box>
+    );
+}
+
+function filterScrperList(records){
+    return records.filter(record => 
+        {
+         return (
+                 (record.getCellValueAsString("Scraper Id") !== "") && 
+                 ((record.getCellValueAsString("L_Hub") == "" ) ||
+                  (record.getCellValueAsString("L_MicroMercados") == "" ) ||
+                  (record.getCellValueAsString("L Municipio") == "" ) ||
+                  (record.getCellValueAsString("L_Estado") == "" ) ||
+                  (record.getCellValueAsString("L_Collection") == "" ) ||
+                  (record.getCellValueAsString("L_Tipo") == "" )
+                  )
+                )
+        })
+}
+
+function dumpData(scrapedRecordsFiltered, table){
+    scrapedRecordsFiltered.forEach(record => {
+        isValidRecord(record) ? setRecordValues(table, record) : console.log("invalid record: " + record.id)
+    });
+}
+
+function isValidRecord(record){
+
+    if(
+        !record.getCellValue("Scraper Id") || 
+        !record.getCellValue("Scraper Price") ||
+        !record.getCellValue("Scraper Description") ||
+        !record.getCellValue("Scraper Location") ||
+        !record.getCellValue("Scraper SQM") ||
+        !record.getCellValue("Scraper Bedrooms") ||
+        !record.getCellValue("Scraper Bathrooms") ||
+        !record.getCellValue("Scraper Gallery") ||
+        !record.getCellValue("Scraper FractionPrice")
+    ){
+        return false;
+    }
+    
+    return true;
+}
+
+function setRecordValues(table, record){
+    const dorms = parseInt(record.getCellValue("Scraper Bedrooms"));
+    const baths = parseInt(record.getCellValue("Scraper Bathrooms"));
+    const area = parseInt(record.getCellValue("Scraper SQM"));
+    const adress = record.getCellValue("Scraper Location");
+    const price = parseInt(record.getCellValue("Scraper Price").replace(/\./g,'').replace(/€/g,''));
+    console.log("Price: " + price);
+
+    table.updateRecordAsync(record,
+        {
+            'Dorms': dorms,
+            'Baths': baths,
+            'Area': area,
+            'T_PVP': price,
+            'Adress': adress,
+            'Creator': {name: 'Scraper'},
+        }
+    );
 }
 
 async function scrapIdealistaUrl(idealistaid){
@@ -80,7 +193,7 @@ function getScraperResultFromMsg(msg){
 }
 
 async function addDatoToView(base, data, idIdealista){
-    const table = base.getTableByName('Web links');
+    const table = base.getTableByName(tableName);
     const amenities = await getAmenities(base, data.amenities);
     console.log(data);
 
@@ -105,7 +218,8 @@ async function addDatoToView(base, data, idIdealista){
         "Scraper Features": getFeatures(data.features)
 
     }).then(function (recordId) {
-        alert(`new record created! ID: ${recordId}`);
+        // alert(`new record created! ID: ${recordId}`);
+        console.log(`new record created! ID: ${recordId}`);
     });
 }
 
@@ -115,7 +229,7 @@ function getGallery(galleryJSON){
 
 async function getAmenities(base, amenitiesJSON){
 
-    let amenitiesTable = base.getTable("Amenities");
+    let amenitiesTable = base.getTable(tableNameAmenities);
     let amenitiesQuery = await amenitiesTable.selectRecordsAsync();
     let amenitiesDict = {};
     for (let record of amenitiesQuery.records) {
